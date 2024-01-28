5a59b870441f44c78f52fb4c8b347b243ea10352
Rustamjon Nuriddinov <rustamcheek11@gmail.com>
2024-01-28 15:08:42 +0500

    app with vue 3

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import MyHello from "./components/MyHello.vue";
+</script>
+
+<template>
+  <MyHello title="Pume" :sena="12500" :added="false" />
+  <MyHello title="Nike" :sena="1252100" :added="true" />
+</template>
